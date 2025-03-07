f6b88b8836034dfedc316dc9f218cede6187ee28
Benned-H <benned1@gmail.com>
2025-03-07 00:45:12 -0500

    Add object-to-YAML method

--- world_model/object_model.py
+++ world_model/object_model.py
@@ -29,3 +29,16 @@ class ObjectModel:
     def type(self) -> str:
         """Get the type of the object."""
         return str(self.message.type.key)
+
+    def get_yaml_data(self) -> dict:
+        """Convert the ObjectModel to a dictionary of its YAML data."""
+        if self.pose.estimated_pose is not None:
+            # Convert pose to a list: [x, y, z, roll, pitch, yaw]
+            xyz, rpy = self.pose.estimated_pose.to_xyz_rpy()
+            pose_list = list(xyz) + list(rpy)
+
+        return {
+            "type": self.type,
+            "pose": pose_list,
+            "frame": self.pose.estimated_pose.ref_frame,
+        }
