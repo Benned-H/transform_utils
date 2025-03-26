41434e8b77616e80e1484542d1ffd05c6aa4a65f
Benned-H <benned1@gmail.com>
2025-03-26 11:20:01 -0400

    Separate 2D and 3D Euclidean distances

--- math/distances.py
+++ math/distances.py
@@ -8,14 +8,14 @@ from transform_utils.kinematics import Pose2D, Pose3D
 from transform_utils.math.angles import wrap_angle_rad
 
 
-def euclidean_distance_m(pose_a: Pose2D | Pose3D, pose_b: Pose2D | Pose3D) -> float:
-    """Compute the Euclidean distance (in meters) between two poses.
+def euclidean_distance_3d_m(pose_a: Pose2D | Pose3D, pose_b: Pose2D | Pose3D) -> float:
+    """Compute the Euclidean distance (in meters) between two poses in 3D space.
 
-    Note: This calculation ignores the orientations of the given poses.
+    Note: This calculation ignores the orientations of the poses.
 
-    :param pose_a: First pose (2D or 3D) used in the distance computation
-    :param pose_b: Second pose (2D or 3D) used in the distance computation
-    :return: Straight-line distance (meters) between the two poses
+    :param pose_a: First pose (2D or 3D) used to compute the 3D distance
+    :param pose_b: Second pose (2D or 3D) used to compute the 3D distance
+    :return: Straight-line distance (meters) between the two poses in 3D space
     """
     if isinstance(pose_a, Pose2D):
         pose_a = Pose3D.from_2d(pose_a)
@@ -23,6 +23,23 @@ def euclidean_distance_m(pose_a: Pose2D | Pose3D, pose_b: Pose2D | Pose3D) -> fl
         pose_b = Pose3D.from_2d(pose_b)
 
     return np.linalg.norm(pose_a.position.to_array(), pose_b.position.to_array())
+
+
+def euclidean_distance_2d_m(pose_a: Pose2D | Pose3D, pose_b: Pose2D | Pose3D) -> float:
+    """Compute the Euclidean distance (in meters) between two poses in 2D space.
+
+    Note: This calculation ignores the orientations and z-values of the poses.
+
+    :param pose_a: First pose (2D or 3D) used to compute the 2D distance
+    :param pose_b: Second pose (2D or 3D) used to compute the 2D distance
+    :return: Straight-line distance (meters) between the two poses in 2D space
+    """
+    if isinstance(pose_a, Pose3D):
+        pose_a = pose_a.to_2d()
+    if isinstance(pose_b, Pose3D):
+        pose_b = pose_b.to_2d()
+
+    return np.linalg.norm(np.array([pose_a.x, pose_a.y]), np.array([pose_b.x, pose_b.y]))
 
 
 def absolute_yaw_error_rad(pose_a: Pose2D | Pose3D, pose_b: Pose2D | Pose3D) -> float:
