355a631086a5038ae7808b1e239dd54bec44a077
Benned-H <benned1@gmail.com>
2025-04-02 18:20:57 -0400

    Clarify sampling_utils and add property-based test

--- src/transform_utils/sampling_utils.py
+++ src/transform_utils/sampling_utils.py
@@ -5,32 +5,27 @@ from __future__ import annotations
 import numpy as np
 
 
-def sample_biased_angles(center_rad: float, num_samples: int = 8) -> list[float]:
-    """Generate angle samples centered around a target angle, alternating outward on either side.
+def generate_alternating_centered_angles(center_rad: float, num_angles: int = 8) -> list[float]:
+    """Generate angle samples that alternate symmetrically from a central target angle.
 
-    Note: The sampled angles will be evenly spaced throughout the interval [0, 2*pi].
+    The function produces a total of `num_angles` samples. It starts with the center angle
+    and alternates by stepping positively and negatively away from the center (in radians).
+    The step size is determined by dividing the full circle [0, 2*pi] by (num_angles + 1).
 
-    :param center_rad: Center angle (radians) around which earlier samples are biased
-    :param num_samples: Total number of angle samples (in radians) to generate
+    :param center_rad: Central angle (in radians) around which to generate samples
+    :param num_angles: Total number of angle samples (in radians) to generate
     :return: List of evenly spaced sampled angles
     """
-    assert num_samples > 0, f"Cannot sample {num_samples} angles."
+    assert num_angles > 0, f"Cannot sample {num_angles} angles."
 
-    step_rad = 2 * np.pi / (num_samples + 1)  # Step (radians) between samples
-    steps_from_center = 1
+    step_rad = 2 * np.pi / (num_angles + 1)  # Step (radians) between samples
+    offset = 1  # Steps from center angle
+    angles = [center_rad]
 
-    samples = [center_rad]
-    while len(samples) < num_samples:
-        greater_rad = center_rad + steps_from_center * step_rad
-        lesser_rad = center_rad - steps_from_center * step_rad
+    while len(angles) < num_angles:
+        angles.append(center_rad + offset * step_rad)
+        if len(angles) < num_angles:
+            angles.append(center_rad + offset * step_rad)
+        offset += 1
 
-        samples.append(greater_rad)
-        samples.append(lesser_rad)
-
-        steps_from_center += 1
-
-    if len(samples) > num_samples:
-        samples.pop()
-
-    assert len(samples) == num_samples, f"Expected {num_samples} angles; found {len(samples)}"
-    return samples
+    return angles[:num_angles]
