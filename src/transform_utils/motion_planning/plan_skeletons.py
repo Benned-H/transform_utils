ef9010d62b05654ce8d98220ad9a3eaafb64eff0
Benned-H <benned1@gmail.com>
2025-04-17 12:57:05 -0400

    Define dataclasses to represent motion plan skeletons

---
+++ src/transform_utils/motion_planning/plan_skeletons.py
@@ -0,0 +1,22 @@
+"""Define dataclasses to represent skeletons of motion plans to be recomputed online."""
+
+from dataclasses import dataclass
+
+from transform_utils.kinematics import Pose3D
+
+
+@dataclass
+class PickPlanSkeleton:
+    """A skeleton for a 'Pick' manipulation plan to be recomputed online."""
+
+    robot_name: str  # Name of the robot to be used to execute the plan
+    manipulator_name: str  # Name of the relevant manipulator
+    target_pose_o_ee: Pose3D  # Target end-effector pose in the object frame
+
+
+@dataclass
+class GoToPlanSkeleton:
+    """A skeleton for a 'GoTo' navigation plan to be recomputed online."""
+
+    robot_name: str  # Name of the robot to be used to execute the plan
+    target_base_pose: Pose3D  # Target robot base pose of the navigation plan
