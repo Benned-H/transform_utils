b2aa2fcb05f3a8544879c3323f25f7f51756ee75
Benned-H <benned1@gmail.com>
2025-04-20 22:36:28 -0400

    Debug tag transforms and TF lookup failures

--- src/transform_utils/perception/tag_tracker.py
+++ src/transform_utils/perception/tag_tracker.py
@@ -2,10 +2,14 @@
 
 import threading
 from dataclasses import dataclass
+from pathlib import Path
 
 import rospy
+import yaml
 from ar_track_alvar_msgs.msg import AlvarMarkers
+from std_srvs.srv import Trigger, TriggerRequest, TriggerResponse
 
+from transform_utils.kinematics import DEFAULT_FRAME
 from transform_utils.kinematics_ros import pose_from_msg
 from transform_utils.perception.april_tag import AprilTagSystem
 from transform_utils.transform_manager import TransformManager
@@ -59,6 +63,8 @@ class TagTracker:
                     ),
                 )
 
+        self._output_srv = rospy.Service("~output_to_yaml", Trigger, self.handle_output_to_yaml)
+
         self._tf_publisher_thread = threading.Thread(target=self._publish_frames_loop)
         self._tf_publisher_thread.daemon = True  # Thread exits when main process does
         self._tf_publisher_thread.start()
@@ -81,12 +87,43 @@ class TagTracker:
             if tag.in_bundle != args.bundle:
                 continue  # Ensure that single-tag detections aren't used for bundled markers
 
-            rospy.loginfo(
-                f"Marker ID {marker.id} (size {tag.size_cm} cm) was detected by camera "
-                f"'{args.camera_name}' with confidence {marker.confidence}.",
+            rospy.loginfo_throttle(
+                period=5,
+                msg=f"Marker {marker.id} ({tag.size_cm} cm) was detected by '{args.camera_name}'.",
             )
 
-            self.tag_system.tags[marker.id].pose = pose_from_msg(marker.pose)  # Update tag's pose
+            pose = pose_from_msg(marker.pose)
+            pose.ref_frame = marker.header.frame_id
+            pose_w_t = TransformManager.convert_to_frame(pose, DEFAULT_FRAME)
+
+            self.tag_system.tags[marker.id].pose = pose_w_t  # Update tag's pose w.r.t. world
+
+    def handle_output_to_yaml(self, _: TriggerRequest) -> TriggerResponse:
+        """Handle a request to output the current object pose estimates to YAML.
+
+        :return: ROS message specifying whether the output process was successful
+        """
+        yaml_path = Path(rospy.get_param("~yaml_output_path"))
+        if yaml_path.suffix not in {".yaml", ".yml"}:
+            message = f"Cannot output YAML to path {yaml_path}; invalid file suffix."
+            return TriggerResponse(success=False, message=message)
+
+        poses_data = {"object_poses": {}, "default_frame": DEFAULT_FRAME}
+        for tag in self.tag_system.tags.values():
+            for object_name in tag.relative_frames:
+                pose_w_o = TransformManager.lookup_transform(object_name, DEFAULT_FRAME)
+                poses_data["object_poses"][object_name] = pose_w_o.to_list()
+
+        yaml_string = yaml.dump(poses_data, sort_keys=True, default_flow_style=True)
+
+        with yaml_path.open(mode="w") as yaml_file:
+            yaml_file.write(yaml_string)
+            yaml_file.close()
+
+        success = yaml_path.exists()
+        message = f"Wrote to file: {yaml_path}." if success else f"Could not write to {yaml_path}."
+
+        return TriggerResponse(success=success, message=message)
 
     def _publish_frames_loop(self) -> None:
         """Publish the known AprilTags' poses (and dependent objects') frames to /tf."""
