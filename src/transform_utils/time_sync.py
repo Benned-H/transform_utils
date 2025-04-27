5f9add365e0db07288af4d85f072ee3488b7d5a1
Benned-H <benned1@gmail.com>
2025-04-27 16:45:59 -0400

    Finalize working TimeSync class

--- src/transform_utils/time_sync.py
+++ src/transform_utils/time_sync.py
@@ -6,11 +6,12 @@ import time
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import Enum
+from math import floor
 
 from google.protobuf.timestamp_pb2 import Timestamp as TimestampProto
 from rospy import Time as ROSTime
 
-from transform_utils.logging import log_error
+from transform_utils.logging import log_error, log_info
 
 NSEC_PER_SEC = 10**9  # Nanoseconds per second
 
@@ -46,19 +47,18 @@ class Timestamp:
         :param clock: System clock corresponding to the given time
         :return: Constructed Timestamp instance
         """
-        rounded_time_s = int(time_s)
-        remaining_time_s = time_s - rounded_time_s
+        whole_seconds = floor(time_s)
+        remaining_time_s = time_s - whole_seconds
         remaining_time_ns = int(remaining_time_s * NSEC_PER_SEC)
 
-        return Timestamp(rounded_time_s, remaining_time_ns, clock)
+        return Timestamp(whole_seconds, remaining_time_ns, clock)
 
-    def to_time_s(self) -> tuple[float, SystemClock]:
+    def to_time_s(self) -> float:
         """Convert the timestamp into a clock time relative to the Unix epoch.
 
-        :return: Tuple (time (sec) since Unix epoch started, relative clock)
+        :return: Time (sec) since Unix epoch started
         """
-        time_s = self.time_s + self.time_ns / NSEC_PER_SEC
-        return (time_s, self.clock)
+        return self.time_s + self.time_ns / NSEC_PER_SEC
 
     @classmethod
     def from_proto(cls, proto: TimestampProto) -> Timestamp:
@@ -67,7 +67,7 @@ class Timestamp:
         :param proto: Protobuf message (assumed to be relative to the robot clock)
         :return: Constructed Timestamp instance
         """
-        return Timestamp(time_s=proto.seconds, time_ns=proto.nanos, clock=SystemClock.ROBOT)
+        return Timestamp(proto.seconds, proto.nanos, SystemClock.ROBOT)
 
     @classmethod
     def from_ros_time(cls, ros_time: ROSTime) -> Timestamp:
@@ -76,7 +76,7 @@ class Timestamp:
         :param ros_time: ROS timestamp representation (assumed to be relative to the local clock)
         :return: Constructed Timestamp instance
         """
-        return Timestamp(time_s=ros_time.secs, time_ns=ros_time.nsecs, clock=SystemClock.LOCAL)
+        return Timestamp(ros_time.secs, ros_time.nsecs, SystemClock.LOCAL)
 
     def add_offset_s(self, offset_s: float) -> Timestamp:
         """Construct a timestamp by adding the given duration (sec) to this timestamp.
@@ -84,13 +84,15 @@ class Timestamp:
         :param offset_s: Duration (seconds) the constructed timestamp is offset from this one
         :return: Constructed Timestamp instance
         """
-        time_s, clock = self.to_time_s()
-        return Timestamp.from_time_s(time_s + offset_s, clock)
+        return Timestamp.from_time_s(self.to_time_s() + offset_s, self.clock)
 
 
 @dataclass
 class SyncResult:
-    """A struct organizing information obtained during a time synchronization."""
+    """A struct organizing information obtained during a time synchronization.
+
+    Meaning of robot_clock_skew_s:      robot_time = local_time + robot_clock_skew_s
+    """
 
     robot_clock_skew_s: float  # Estimated robot clock skew from the local clock (seconds)
     round_trip_time_s: float  # Current duration (seconds) of a request-response trip
@@ -101,6 +103,8 @@ class TimeSync(ABC):
 
     def __init__(self) -> None:
         """Initialize member variables to their initial values."""
+        self.resync_depth_limit = 3  # Maximum number of repeated attempts to time-sync with robot
+
         self.latest_sync_result: SyncResult | None = None  # Result from latest time-sync
 
         # Maximum duration (seconds) of any request-response round trip
@@ -111,7 +115,7 @@ class TimeSync(ABC):
         self.total_sync_time_s = 0.0  # Total time (seconds) spent synchronizing with the robot
         self.total_sync_count = 0  # Number of completed calls to resync()
 
-        self.resync()
+        self._origin_timestamp = Timestamp.now()
 
     @abstractmethod
     def _sync(self) -> SyncResult | None:
@@ -120,27 +124,38 @@ class TimeSync(ABC):
         :return: Latest data from robot time-sync (None if synchronization fails)
         """
 
-    def resync(self) -> None:
-        """Resynchronize the local clock with the robot clock."""
-        start_time_s = time.time()
+    def resync(self, attempts_completed: int = 0) -> None:
+        """Resynchronize the local clock with the robot clock.
+
+        :param attempts_completed: Re-sync attempt depth, to avoid endless re-syncs (begins at 0)
+        """
+        if attempts_completed >= self.resync_depth_limit:
+            log_error(f"Unable to time-sync with robot after {attempts_completed} attempts.")
+            return
+
+        start_time_s = time.perf_counter()
         self.latest_sync_result = self._sync()
-        end_time_s = time.time()
+        elapsed_s = time.perf_counter() - start_time_s
 
-        sync_duration_s = end_time_s - start_time_s
-        if self.max_sync_duration_s is None:
-            self.max_sync_duration_s = sync_duration_s
-        else:
-            self.max_sync_duration_s = max(self.max_sync_duration_s, sync_duration_s)
+        # Reject bad or overly stale time-sync estimates
+        if self.latest_sync_result is None:
+            log_info("Result from time-sync was None; trying again...")
+            self.resync(attempts_completed + 1)
+            return
 
-        if self.max_round_trip_s is None:
-            self.max_round_trip_s = self.latest_sync_result.round_trip_time_s
-        else:
-            self.max_round_trip_s = max(
-                self.max_round_trip_s,
-                self.latest_sync_result.round_trip_time_s,
-            )
+        self.max_sync_duration_s = (
+            elapsed_s
+            if self.max_sync_duration_s is None
+            else max(elapsed_s, self.max_sync_duration_s)
+        )
 
-        self.total_sync_time_s += sync_duration_s
+        self.max_round_trip_s = (
+            self.latest_sync_result.round_trip_time_s
+            if self.max_round_trip_s is None
+            else max(self.latest_sync_result.round_trip_time_s, self.max_round_trip_s)
+        )
+
+        self.total_sync_time_s += elapsed_s
         self.total_sync_count += 1
 
     @property
@@ -149,7 +164,7 @@ class TimeSync(ABC):
 
         :return: Average duration (seconds) of calls to resync(), else None if no calls yet
         """
-        if self.total_sync_time_s == 0.0 or self.total_sync_count == 0:
+        if self.total_sync_count == 0:
             return None
         return self.total_sync_time_s / self.total_sync_count
 
@@ -164,20 +179,20 @@ class TimeSync(ABC):
             return ts
 
         if self.latest_sync_result is None:
-            log_error("Cannot convert relative clock; robot clock skew is unknown.")
+            log_error("Robot clock skew unknown - cannot convert timestamps.")
             return None
 
-        time_s, ts_clock = ts.to_time_s()
+        time_s = ts.to_time_s()
 
-        if ts_clock == SystemClock.LOCAL and rel_clock == SystemClock.ROBOT:
+        if ts.clock == SystemClock.LOCAL and rel_clock == SystemClock.ROBOT:
             robot_time_s = time_s + self.latest_sync_result.robot_clock_skew_s
             return Timestamp.from_time_s(robot_time_s, SystemClock.ROBOT)
 
-        if ts_clock == SystemClock.ROBOT and rel_clock == SystemClock.LOCAL:
+        if ts.clock == SystemClock.ROBOT and rel_clock == SystemClock.LOCAL:
             local_time_s = time_s - self.latest_sync_result.robot_clock_skew_s
             return Timestamp.from_time_s(local_time_s, SystemClock.LOCAL)
 
-        log_error(f"Unable to convert timestamp relative to {ts_clock} to clock {rel_clock}.")
+        log_error(f"Unable to convert timestamp relative to {ts.clock} to clock {rel_clock}.")
         return None
 
     def convert_to_robot_proto(self, ts: Timestamp | ROSTime) -> TimestampProto | None:
@@ -205,18 +220,51 @@ class TimeSync(ABC):
             ts = Timestamp.from_proto(ts)
 
         local_ts = self.change_relative_clock(ts, rel_clock=SystemClock.LOCAL)
-        local_time_s, _ = local_ts.to_time_s()
-        return None if local_ts is None else ROSTime.from_seconds(local_time_s)
 
-    def get_future_ref_time_proto(self) -> TimestampProto:
-        """Create a Protobuf message marking a future reference time for a robot command.
+        return None if local_ts is None else ROSTime.from_sec(local_ts.to_time_s())
 
-        :return: Constructed Timestamp Protobuf message
+    def get_future_margin_s(self) -> TimestampProto | None:
+        """Compute a safe margin (in seconds) to send commands early to Spot.
+
+        :return: Duration (seconds) reflecting observed round-trip times
         """
-        future_proof_s = 2.0 * self.max_round_trip_s + 0.1
-        ref_time_local_ts = Timestamp.now().add_offset_s(future_proof_s)
+        if self.latest_sync_result is None:
+            self.resync()
 
-        # Offset by the opposite of the robot clock skew (i.e., "toward local" one extra skew)
-        #   so that when the Spot SDK does so with the reference time, things are correct
-        adjusted_ts = ref_time_local_ts.add_offset_s(-self.latest_sync_result.robot_clock_skew_s)
+        if self.latest_sync_result is None or self.max_round_trip_s is None:
+            log_error("Unknown time-sync; cannot compute future reference time Protobuf.")
+            return None
+
+        return 2.0 * self.max_round_trip_s + 0.1
+
+    def log_sync_state(self) -> None:
+        """Log the current state of the time-sync to standard output."""
+        if self.latest_sync_result is None:
+            log_info("Latest time-sync result is None; robot clock skew is unknown.")
+        else:
+            clock_skew_s = self.latest_sync_result.robot_clock_skew_s
+            log_info(f"Current robot clock skew from local: {clock_skew_s:.4f} seconds.")
+
+            round_trip_time_s = self.latest_sync_result.round_trip_time_s
+            log_info(f"Current time-sync round trip time: {round_trip_time_s:.4f} seconds.")
+
+        if self.max_round_trip_s is not None:
+            log_info(f"Maximum observed round trip time: {self.max_round_trip_s:.4f} seconds.")
+
+        if self.max_sync_duration_s is not None:
+            max_sync_time_s = self.max_sync_duration_s
+            log_info(f"Maximum duration any time-sync has taken: {max_sync_time_s:.4f} seconds.")
+
+        avg_sync_time_s = self.average_sync_duration_s
+        if avg_sync_time_s is not None:
+            log_info(f"Average duration per successful time-sync: {avg_sync_time_s:.4f} seconds.")
+
+    def add_proto_offset_s(self, proto: TimestampProto, offset_s: float) -> TimestampProto | None:
+        """Create a new Timestamp proto by adding the given duration (s) to the given proto.
+
+        :param proto: Timestamp Protobuf message representing a time relative to the robot clock
+        :param offset_s: Duration (seconds) added to the timestamp
+        :return: Constructed Timestamp Protobuf message, or None if conversion fails
+        """
+        adjusted_ts = Timestamp.from_proto(proto).add_offset_s(offset_s)
         return self.convert_to_robot_proto(adjusted_ts)
